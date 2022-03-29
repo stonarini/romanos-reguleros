5c44b603b5ff50b4c84988fce17e1f2278054c82
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-30 00:28:43 +0200

    test: parametrizar test , alugnos no necesario pero practica

--- romans/src/test/java/edu/poniperro/NumeroRomanoTest.java
+++ romans/src/test/java/edu/poniperro/NumeroRomanoTest.java
@@ -28,6 +28,7 @@ public class NumeroRomanoTest {
         // String testCase = "M";
         NumeroRomano numeroRomano = new NumeroRomano(roman);
         assertEquals(decimal, numeroRomano.toDecimal());
+
         /**
          * El caso MMMM es control de errores
          * y no puede estas en el test de la logica
@@ -36,154 +37,94 @@ public class NumeroRomanoTest {
          */
     }
 
-    // @Test
-    // public void tres_repeticiones_C_test() {
+    @ParameterizedTest
+    @CsvSource({
+            "3300, UMMMUCCCU",
+            "3030, UMMMUXXXU",
+            "3003,UMMMUIIIU"
+    })
+    public void tres_repeticiones_C_X_I_test(int decimal, String roman) {
+        NumeroRomano numeroRomano = new NumeroRomano(roman);
+        assertEquals(decimal, numeroRomano.toDecimal());
+    }
 
-    // String testCase = "UMMMUCCCU";
-    // NumeroRomano numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(3300, numeroRomano.toDecimal(), 0);
-    // }
+    @Test
+    public void una_D_test() {
 
-    // @Test
-    // public void tres_repeticiones_X_test() {
+        String testCase = "UMMMUDUIIIU";
+        NumeroRomano numeroRomano = new NumeroRomano(testCase);
+        assertEquals(3503, numeroRomano.toDecimal());
 
-    // String testCase = "UMMMUXXXU";
-    // NumeroRomano numeroRomano = new NumeroRomano(testCase);
+        testCase = "MMMUCDUIIIU";
+        numeroRomano = new NumeroRomano(testCase);
+        assertNotEquals(3503, numeroRomano.toDecimal());
+    }
 
-    // assertEquals(3030, numeroRomano.toDecimal(), 0);
-    // }
+    /**
+     * Grupos substractivos
+     * IV(4), IX(9),
+     * XL(40), XC(90),
+     * CD(400), CM(900)
+     */
 
-    // @Test
-    // public void tres_repeticiones_I_test() {
+    @ParameterizedTest
+    @CsvSource({
+            "400, UCDU",
+            "900, UCMU"
+    })
+    public void grupo_C_DM_test(int decimal, String roman) {
+        NumeroRomano numeroRomano = new NumeroRomano(roman);
+        assertEquals(decimal, numeroRomano.toDecimal());
 
-    // String testCase = "UMMMUIIIU";
-    // NumeroRomano numeroRomano = new NumeroRomano(testCase);
+    }
 
-    // assertEquals(3003, numeroRomano.toDecimal(), 0);
-    // }
+    @ParameterizedTest
+    @CsvSource({
+            "40, UXLU",
+            "90, UXCU"
+    })
+    public void grupo_X_LC_test(int decimal, String roman) {
+        NumeroRomano numeroRomano = new NumeroRomano(roman);
+        assertEquals(decimal, numeroRomano.toDecimal());
+    }
 
-    // @Test
-    // public void una_D_test() {
+    @ParameterizedTest
+    @CsvSource({
+            "4, UIVU",
+            "9, UIXU"
+    })
+    public void grupo_I_VX_test(int decimal, String roman) {
+        NumeroRomano numeroRomano = new NumeroRomano(roman);
+        assertEquals(decimal, numeroRomano.toDecimal());
 
-    // String testCase = "UMMMUDUIIIU";
-    // NumeroRomano numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(3503, numeroRomano.toDecimal(), 0);
+    }
 
-    // testCase = "MMMUCDUIIIU";
-    // numeroRomano = new NumeroRomano(testCase);
-    // assertNotEquals(3503, numeroRomano.toDecimal(), 0);
-    // }
+    @ParameterizedTest
+    @CsvSource({
+            "3888, MMMDCCCLXXXVIII"
+    })
+    public void grupos_sumatorios_tres_digitos_test(int decimal, String roman) {
+        NumeroRomano numeroRomano = new NumeroRomano(roman);
+        assertEquals(decimal, numeroRomano.toDecimal());
+    }
 
-    // /**
-    // * Grupos substractivos
-    // * IV(4), IX(9),
-    // * XL(40), XC(90),
-    // * CD(400), CM(900)
-    // */
+    @ParameterizedTest
+    @CsvSource({
+            "2777, MMDCCLXXVII"
+    })
+    public void grupos_sumatorios_test(int decimal, String roman) {
+        NumeroRomano numeroRomano = new NumeroRomano(roman);
+        assertEquals(decimal, numeroRomano.toDecimal());
+    }
 
-    // @Test
-    // public void grupo_C_DM_test() {
+    @ParameterizedTest
+    @CsvSource({
+            "444, CDXLIV",
+            "439, CDXXXIX"
+    })
+    public void grupos_substractivos_test(int decimal, String roman) {
+        NumeroRomano numeroRomano = new NumeroRomano(roman);
+        assertEquals(decimal, numeroRomano.toDecimal());
+    }
 
-    // String testCase = "UCDU";
-    // NumeroRomano numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(400, numeroRomano.toDecimal(), 0);
-
-    // testCase = "UCMU";
-    // numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(900, numeroRomano.toDecimal(), 0);
-    // }
-
-    // @Test
-    // public void grupo_X_LC_test() {
-
-    // String testCase = "UXLU";
-    // NumeroRomano numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(40, numeroRomano.toDecimal(), 0);
-
-    // testCase = "UXCU";
-    // numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(90, numeroRomano.toDecimal(), 0);
-    // }
-
-    // @Test
-    // public void grupo_I_VX_test() {
-
-    // String testCase = "UIVU";
-    // NumeroRomano numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(4, numeroRomano.toDecimal(), 0);
-
-    // testCase = "UIXU";
-    // numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(9, numeroRomano.toDecimal(), 0);
-    // }
-
-    // @Test
-    // public void grupos_sumatorios_tres_digitos_test() {
-    // String testCase = "MMMDCCCLXXXVIII"; // 3888
-    // NumeroRomano numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(3888, numeroRomano.toDecimal(), 0);
-    // }
-
-    // @Test
-    // public void grupos_sumatorios_test() {
-    // String testCase = "MMDCCLXXVII"; // 2777
-    // NumeroRomano numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(2777, numeroRomano.toDecimal(), 0);
-    // }
-
-    // @Test
-    // public void grupos_substractivos_test() {
-    // String testCase = "CDXLIV"; // 444
-    // NumeroRomano numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(444, numeroRomano.toDecimal(), 0);
-
-    // testCase = "CDXXXIX"; // 439
-    // numeroRomano = new NumeroRomano(testCase);
-    // assertEquals(439, numeroRomano.toDecimal(), 0);
-    // }
-
-    // // @Test
-    // // public void initArrayRegex_test() {
-    // // String test = "V";
-    // // numeroRomano.setNumeroRomano(test);
-    // // assertEquals(2, numeroRomano.getRegexDiccionario().longitud());
-    // // assertEquals(5, numeroRomano.valorDecimal(test));
-    // // assertEquals("(?<!C)[DM]|(?<!X)[LC](?![DM])|(?<!I)[VX](?![LC])|I(?![VX])",
-    // // numeroRomano.getRegexDiccionario().getRegexValue("grupoSumatorio"));
-    // // assertEquals("(C[DM])|(X[LC])|(I[VX])",
-    // // numeroRomano.getRegexDiccionario().getRegexValue("grupoSustractivo"));
-    // // }
-
-    // // @Test
-    // // public void toDecimal() {
-    // // String test = "V";
-    // // numeroRomano.setNumeroRomano(test);
-    // // assertEquals(2, numeroRomano.getExpresionesRegulares().size());
-    // // assertTrue(numeroRomano.getRegexDiccionario().getValues()
-    // // .contains("(?<!C)[DM]|(?<!X)[LC](?![DM])|(?<!I)[VX](?![LC])|I(?![VX])"));
-    // //
-    // assertTrue(numeroRomano.getRegexDiccionario().getValues().contains("(C[DM])|(X[LC])|(I[VX])"));
-    // // }
-
-    // // @Test
-    // // public void valorDecimal_test() {
-    // // String test = "V";
-    // // numeroRomano.setNumeroRomano(test);
-    // // assertEquals(2, numeroRomano.getRegexDiccionario().getRegex().size());
-    // // assertEquals(5, numeroRomano.valorDecimal(test));
-
-    // // test = "IV";
-    // // numeroRomano.setNumeroRomano(test);
-    // // assertEquals(4, numeroRomano.valorDecimal(test));
-
-    // // test = "CM";
-    // // numeroRomano.setNumeroRomano(test);
-    // // assertEquals(900, numeroRomano.valorDecimal(test));
-
-    // /**
-    // * test = "U";
-    // * numeroRomano.setNumeroRomano("U");
-    // * assertEquals(900, numeroRomano.valorDecimal(test));
-    // */
-    // // }
 }
